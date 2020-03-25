291d1614d4d410c0c5b441114233786f86b84295
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-03-24 19:16:38 -0500

    gets farmer with cows array

---
+++ src/javascripts/helpers/data/farmerCowData.js
@@ -0,0 +1,22 @@
+import Axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getFarmerCowsByFarmerUid = (uid) => new Promise((resolve, reject) => {
+  Axios.get(`${baseUrl}/farmerCows.json?orderBy="farmerUid"&=equalTo="${uid}"`)
+  // query string, orderBy "name of the key", & equalTo "(something here if you want specifics)"
+    .then((response) => {
+      const demFarmerCows = response.data;
+      const farmerCows = [];
+      // going to return something like: ['farmerCow1', 'farmerCow2'].forEach()
+      Object.keys(demFarmerCows).forEach((farmerCowId) => {
+        demFarmerCows[farmerCowId].id = farmerCowId;
+        farmerCows.push(demFarmerCows[farmerCowId]);
+      });
+      resolve(farmerCows);
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getFarmerCowsByFarmerUid };
